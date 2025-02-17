50726630a52109ba153c2e052363380924b2a701
aneroodh <cv245@snu.edu.in>
2025-02-17 16:40:21 +0530

    v1.1

--- test.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-axios.get('http://localhost:5000/loadData', {
-    params: { user: "aneroodh14" }
-  })
-  .then(response => console.log(response.data))
-  .catch(error => console.error('Error:', error));
-
-// axios.get('http://localhost:5000/connection')
